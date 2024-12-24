9e17ae7b781a616b73e0ec75cc037337e6d0c6a0
AlexShkn <barsuk-b@yandex.ru>
2024-12-24 22:20:48 +0300

    поиск товаров

--- soloprom-next/src/components/Header/HeaderSearch.tsx
+++ soloprom-next/src/components/Header/HeaderSearch.tsx
@@ -5,32 +5,48 @@ import { DebouncedFunction } from '@/supports/debounce'
 import { debounce } from '@/supports/debounce'
 
 import { cardDataProps } from '../ProductList/ProductList'
+import Link from 'next/link'
 
 const HeaderSearch = () => {
   const [searchValue, setSearchValue] = useState<string>('')
   const [products, setProducts] = useState<cardDataProps[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
+  // Асинхронная функция для поиска
   const searchProductsForValue = async (name: string): Promise<void> => {
     if (name.trim() === '') {
       setProducts([])
       return
     }
 
-    const response = await searchProducts('name', name)
-    const data: cardDataProps[] = await response.data
-    setProducts(data)
+    try {
+      setIsLoading(true)
+      const response = await searchProducts('name', name)
+      const data: cardDataProps[] = await response.data
+      setProducts(data)
+    } catch (error) {
+      console.error('An error occurred while searching:', error)
+      setProducts([])
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   const debouncedSearch: DebouncedFunction<(value: string) => Promise<void>> =
     debounce((value) => searchProductsForValue(value), 300)
 
-  // Используем useEffect для запуска дебаунс-поиска
   useEffect(() => {
-    debouncedSearch(searchValue) // Изменяем на searchValue вместо name
+    debouncedSearch(searchValue)
     return () => {
       debouncedSearch.cancel && debouncedSearch.cancel()
     }
   }, [searchValue])
+
+  const resetSearch = () => {
+    setSearchValue('')
+    setProducts([])
+    setIsLoading(true)
+  }
 
   return (
     <div className="header-bottom__catalog-field relative flex max-w-[600px] flex-auto items-center">
@@ -39,6 +55,7 @@ const HeaderSearch = () => {
           type="text"
           name="search-product"
           id="search-product-input"
+          value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
           placeholder="Поиск по наименованию и размеру"
           className="rounded-tl-4 rounded-bl-4 h-full w-full bg-[#f4f5fa] px-8 py-4 placeholder:text-sm placeholder:text-[#c2c5da]"
@@ -58,21 +75,42 @@ const HeaderSearch = () => {
       <button
         type="button"
         id="search-product-btn"
+        onClick={resetSearch} // Добавляем обработчик клика для очистки
         className="button rounded-tr-4 rounded-br-4 px-8 py-4"
       >
-        <span>Найти</span>
+        <span> {searchValue ? 'Сбросить' : 'Найти'} </span>
         <svg className="icon hidden h-5 w-5 fill-white">
           <use xlinkHref="/img/sprite.svg#search"></use>
         </svg>
       </button>
 
-      <div className="header-bottom__catalog-search-wrapper absolute left-0 hidden w-full overflow-hidden">
-        <CloseButton
-          classNames="header-bottom__catalog-search-close window-close-btn"
-          iconClass="icon-close"
-        />
-        <ul className="header-bottom__catalog-search-list scroll-bar flex h-full max-h-[50vh] min-h-[150px] w-full flex-col overflow-y-auto overflow-x-hidden rounded bg-white shadow-custom"></ul>
-      </div>
+      {searchValue && (
+        <div className="absolute left-0 top-[100%] mt-1 w-full overflow-hidden">
+          <ul
+            className={`header-bottom__catalog-search-list scroll-bar flex h-full max-h-[50vh] w-full flex-col overflow-y-auto overflow-x-hidden rounded bg-white shadow-custom ${isLoading && 'load'}`}
+          >
+            {searchValue && products.length
+              ? products.map((item) => (
+                  <li
+                    key={item.id}
+                    className="header-bottom__catalog-search-item link-hover w-full transition-all even:bg-[#e0e0e0]"
+                  >
+                    <Link
+                      href={item.url}
+                      className="inline-flex w-full items-center justify-between gap-2.5 px-4 py-2 hover:bg-hoverBlue hover:text-white"
+                    >
+                      <span>{item.name}</span>
+                    </Link>
+                  </li>
+                ))
+              : !isLoading && (
+                  <li className="block h-full w-full p-5 text-center">
+                    Ничего не найдено
+                  </li>
+                )}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
