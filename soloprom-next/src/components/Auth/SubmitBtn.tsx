fb1cbc43c8b82d1499be99377e3a3807230f657e
AlexShkn <barsuk-b@yandex.ru>
2025-01-06 15:10:59 +0300

    фиксация перед auth client

--- soloprom-next/src/components/Auth/SubmitBtn.tsx
+++ soloprom-next/src/components/Auth/SubmitBtn.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 
 interface Props {
   text: string
+  disabled?: boolean
 }
 
-export const SubmitBtn: React.FC<Props> = ({ text }) => {
+export const SubmitBtn: React.FC<Props> = ({ text, disabled }) => {
   return (
     <button
       type="submit"
-      disabled={true}
-      className="button h-10 w-full cursor-pointer bg-accentBlue font-medium text-white"
+      disabled={disabled}
+      className="button h-10 w-full cursor-pointer font-medium text-white enabled:bg-accentBlue disabled:bg-grayColor"
     >
       {text}
     </button>
